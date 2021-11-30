30254d5f6535e47c578c90019134a16231d58966
Mike Knoles <MKnoles78@gmail.com>
2021-11-29 23:17:14 -0500

    added base pseudocode

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,4 +1,16 @@
 //GradeBook class using an array to store test grades
 public class GradeBook {
 
+	private String courseName; // name of course this GradeBook represents
+
+	private int[] grades; // array of student grades
+
+	// constructor
+
+	// method to set the course name
+
+	// method to retrieve the course name
+
+	// perform various operations on the data
+
 }
