ae3d20370d5631d862ccb2aad9e15b94b56b14c0
Mike Knoles <MKnoles78@gmail.com>
2021-11-30 23:30:26 -0500

    added ouputBarChart() method to display grades in bar chart

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -78,5 +78,32 @@ public class GradeBook {
 	}
 
 	// output bar chart displaying grade distribution
+	public void outputBarChart() {
+		System.out.println("Grade distribution:");
 
+		// stores frequency of grades in each range of 10 grades
+		int[] frequency = new int[11];
+
+		// for each grade, increment the appropriate frequency
+		for (int grade : grades) {
+			++frequency[grade / 10];
+		}
+
+		// for each grade frequency, print bar in chart
+		for (int count = 0; count < frequency.length; count++) {
+			// output bar label ("00-09: ", ..., "90-99: ", "100: ")
+			if (count == 10) {
+				System.out.printf("%5d: ", 100);
+			} else {
+				System.out.printf("%02d-%02d: ", count * 10, count * 10 + 9);
+			}
+
+			// print bar of asterisks
+			for (int stars = 0; stars < frequency[count]; stars++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
 }
