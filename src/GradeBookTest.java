85079a1d067766482b5647bb69f6882515d89f3a
Mike Knoles <MKnoles78@gmail.com>
2021-11-30 23:56:08 -0500

    built out main to use a list of grades and display in console

--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -3,7 +3,12 @@
 public class GradeBookTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// array of student grades
+		int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+
+		GradeBook myGradeBook = new GradeBook("CS101 Introduction to Java Programming", gradesArray);
+		System.out.printf("Welcome to the grade book for %n%s%n%n", myGradeBook.getCourseName());
+		myGradeBook.processGrades();
 
 	}
 
