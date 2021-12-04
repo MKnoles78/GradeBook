80a05b490e477ea9bd8eab7c66da72244221dd33
Mike Knoles <MKnoles78@gmail.com>
2021-12-03 23:51:43 -0500

    added getMaximum

--- src/GradeBookRefactor.java
+++ src/GradeBookRefactor.java
@@ -55,6 +55,24 @@ public class GradeBookRefactor {
 			return lowGrade;
 		}
 
+		// find maximum grade
+		public int getMaximum() {
+			// assume first element of grades array is largest
+			int highGrade = grades[0][0];
+
+			// loop through rows of grades array
+			for (int[] studentGrades : grades) {
+				// loop through columns of current row
+				for (int grade : studentGrades) {
+					// if grade greater than highGrade, assign it to highGrade
+					if (grade > highGrade) {
+						highGrade = grade;
+					}
+				}
+			}
+			return highGrade;
+		}
+
 		int lowGrade = grades[0]; // assume grades[0] is smallest
 
 		// loop through grades array
